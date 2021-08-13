9af37ac7711913c0cebee483d3b0d2c0aaeed5e5
Trace <tracedelange@me.com>
2021-08-13 12:00:50 -0500

    commit tree 0c8b660578a99710fef865bf6b245e06ba2d0e18

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
 
+class CashRegister
+
+    attr_accessor :total, :items, :last_transaction
+    attr_reader :discount
+
+
+    def initialize(discount=nil)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quantity=1)
+        self.total += price * quantity
+        @items.fill(title, @items.size, quantity)
+        @last_transaction = [title, price, quantity]
+    end
+
+    def apply_discount
+        if !!discount
+            self.total = self.total - (self.total * ((discount).to_f/100).to_f)
+            return "After the discount, the total comes to $#{total.to_i}."
+        else
+            return "There is no discount to apply."
+        end
+    end
+
+    def items
+        return @items
+    end
+
+    def void_last_transaction
+        @total = @total - (@last_transaction[1] * @last_transaction[2])
+
+    end
+
+end
